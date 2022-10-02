963e6b6c2291b47bdebaade24b43a1cf7fa8c04e
Maxim <bmw_077@mail.ru>
2022-10-02 19:15:28 +0300

    crud axios

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -30,6 +30,5 @@ class PersonController extends Controller
         ];
 
         return $persons;
-
     }
 }
--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,30 +1,31 @@
 <template>
-    <div>
+    <div class="w-25">
+        <CreateComponent></CreateComponent>
         <SinglePostComponent></SinglePostComponent>
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Name</th>
-                <th scope="col">Age</th>
-                <th scope="col">Job</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="person in persons">
-                <th scope="row">{{ person.id }}</th>
-                <td>{{ person.name }}</td>
-                <td>{{ person.age }}</td>
-                <td>{{ person.job }}</td>
-            </tr>
-            </tbody>
-        </table>
+<!--        <table class="table">-->
+<!--            <thead>-->
+<!--            <tr>-->
+<!--                <th scope="col">Id</th>-->
+<!--                <th scope="col">Name</th>-->
+<!--                <th scope="col">Age</th>-->
+<!--                <th scope="col">Job</th>-->
+<!--            </tr>-->
+<!--            </thead>-->
+<!--            <tbody>-->
+<!--            <tr v-for="person in persons">-->
+<!--                <th scope="row">{{ person.id }}</th>-->
+<!--                <td>{{ person.name }}</td>-->
+<!--                <td>{{ person.age }}</td>-->
+<!--                <td>{{ person.job }}</td>-->
+<!--            </tr>-->
+<!--            </tbody>-->
+<!--        </table>-->
     </div>
 </template>
 
 <script>
 import SinglePostComponent from "./SinglePostComponent";
-
+import CreateComponent from "./CreateComponent";
 export default {
     name: "PostComponent",
 
@@ -35,25 +36,12 @@ export default {
     },
 
     mounted() {
-        this.getPersons();
     },
 
     methods: {
-        getPersons() {
-            axios.get('/persons')
-                .then(res => {
-                    this.persons = res.data;
-                })
-                .catch(error => {
-
-                })
-                .finally({
-
-                })
-        }
     },
 
-    components: {SinglePostComponent},
+    components: {SinglePostComponent, CreateComponent},
 }
 </script>
 
