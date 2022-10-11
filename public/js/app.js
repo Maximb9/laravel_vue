1866d5941d5980fc497d49e66d46c319fb2305ff
Maxim <bmw_077@mail.ru>
2022-10-11 23:27:42 +0300

    store controller vue

--- public/js/app.js
+++ public/js/app.js
@@ -5075,7 +5075,13 @@ var render = function render() {
         name: "person.index"
       }
     }
-  }, [_vm._v("People")]), _vm._v(" "), _c("router-view")], 1);
+  }, [_vm._v("People")]), _vm._v(" "), _c("router-link", {
+    attrs: {
+      to: {
+        name: "person.create"
+      }
+    }
+  }, [_vm._v("Create")]), _vm._v(" "), _c("router-view")], 1);
 };
 
 var staticRenderFns = [];
@@ -5171,6 +5177,12 @@ vue__WEBPACK_IMPORTED_MODULE_0__["default"].use(vue_router__WEBPACK_IMPORTED_MOD
       return __webpack_require__.e(/*! import() */ "resources_js_components_Person_Index_vue").then(__webpack_require__.bind(__webpack_require__, /*! ./components/Person/Index */ "./resources/js/components/Person/Index.vue"));
     },
     name: 'person.index'
+  }, {
+    path: '/people/create',
+    component: function component() {
+      return __webpack_require__.e(/*! import() */ "resources_js_components_Person_Create_vue").then(__webpack_require__.bind(__webpack_require__, /*! ./components/Person/Create */ "./resources/js/components/Person/Create.vue"));
+    },
+    name: 'person.create'
   }]
 }));
 
@@ -43159,7 +43171,7 @@ Vue.compile = compileToFunctions;
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames not based on template
-/******/ 			if (chunkId === "resources_js_components_Person_Index_vue") return "js/" + chunkId + ".js";
+/******/ 			if ({"resources_js_components_Person_Index_vue":1,"resources_js_components_Person_Create_vue":1}[chunkId]) return "js/" + chunkId + ".js";
 /******/ 			// return url for filenames based on template
 /******/ 			return undefined;
 /******/ 		};
--- resources/js/components/Person/Create.vue
+++ resources/js/components/Person/Create.vue
@@ -1,12 +1,44 @@
 <template>
-<div>
-    create
+<div class="w-25">
+    <div class="mb-3">
+        <input type="text" v-model="name" placeholder="name" class="form-control">
+    </div>
+    <div class="mb-3">
+        <input type="number" v-model="age" placeholder="age" class="form-control">
+    </div>
+    <div class="mb-3">
+        <input type="text" v-model="job" placeholder="job" class="form-control">
+    </div>
+    <div class="mb-3">
+        <input @click.prevent="store" type="submit" value="add" class="btn btn-primary">
+    </div>
 </div>
 </template>
 
 <script>
 export default {
-    name: "Create"
+    name: "Create",
+
+    data(){
+        return {
+            name: null,
+            age: null,
+            job: null,
+        }
+    },
+
+    methods: {
+        store() {
+            axios.post('/api/people', {
+                name: this.name,
+                age: this.age,
+                job: this.job
+            })
+                .then(res => {
+
+                })
+        }
+    }
 }
 </script>
 
